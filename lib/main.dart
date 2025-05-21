cd4215ddd382600ca540c9005ec2c1c5b8d7bc20
rahmahaisyah <rahmahasyh@gmail.com>
2025-05-21 11:06:13 +0700

    refactor: adjust margin and padding for better layout alignment

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/shared/theme/app_style.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Clypsera",
       initialRoute: AppPages.initial,
       getPages: AppPages.routes,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Style.whiteColor,
+      ),
     ),
   );
 }
